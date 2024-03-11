65e4950a1680400cd2ddc36a20e6f5e335bf201c
Klavicodes <ejibeclaver0@gmail.com>
2024-03-11 03:43:20 +0000

    exercise 13c

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -65,6 +65,8 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
         matchingItem = item;
       }
     });
+
+    const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
     
     if (matchingItem) {
       matchingItem.quantity += 1;
