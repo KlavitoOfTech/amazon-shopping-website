3dcd12cf23b966f3faf5559c982731383de9c555
Klavicodes <ejibeclaver0@gmail.com>
2024-03-11 04:37:01 +0000

    exercise 13h

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -56,7 +56,7 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
-    const productId = button.dataset.productId;
+    const {productId} = button.dataset;
 
     let matchingItem;
 
@@ -74,8 +74,8 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       matchingItem.quantity += quantity;
     } else {
       cart.push({
-        productId: productId,
-        quantity: quantity
+        productId,
+        quantity
       });
     }
 
