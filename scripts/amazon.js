4283f4e10ff3b941af0fbdc75df58891633fa7f3
Klavicodes <ejibeclaver0@gmail.com>
2024-03-19 17:00:25 +0100

    Save data and Generate HTML for delivery options

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart, addToCart} from '../data/cart.js';
+import {cart, addToCart, calculateCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -58,15 +58,13 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML
 
-function updateCart() {
-  let cartQuantity = 0;
+updateCartQuantity();
 
-  cart.forEach((item) => {
-    cartQuantity += item.quantity;
-  });
+function updateCartQuantity() {
+  const cartQuantity = calculateCartQuantity();
 
   document.querySelector('.js-cart-quantity')
-    .innerHTML = cartQuantity;
+  .innerHTML = cartQuantity;
 };
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
@@ -95,7 +93,7 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
 
     addToCart(productId);
 
-    updateCart();   
+    updateCartQuantity();   
 
     const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`); 
 
